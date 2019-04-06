f2ea980f46cc7a87dda0ad59123d09d24c8baf47
user <51wanderer51@gmail.com>
2019-04-06 14:44:11 -0400

    Add star db function

--- db.js
+++ db.js
@@ -13,23 +13,26 @@ module.exports = {
 	},
 
 	listNotes: function(limit, cb) {
-		connection.query('SELECT uuid, author, locked, rating, time FROM notes WHERE hidden = 0', function(error, results, fields) {
-			if(error) throw error;
+		connection.query('SELECT uuid, author, locked, rating, time FROM notes WHERE hidden = 0', function (error, results, fields) {
 			return cb(error, results);
 		});
 	},
 
 	getNote: function(note, cb) {
 		connection.query('SELECT uuid, author, locked, rating, time FROM notes WHERE uuid = ?', [note], function (error, results, fields) {
-  			if (error) throw error;
   			return cb(error, results);
 		});	
 	},
 
 	insertNote: function(uuid, author, cb) {
 		connection.query('INSERT INTO notes (uuid, author) VALUES (?, ?)', [uuid, author], function (error, results, fields) {
-			if(error) throw error;
 			return cb(error);
 		});
 	},
+
+	addStar: function(uuid, stars cb) {
+		connection.query('UPDATE notes SET rating = rating + ? WHERE uuid = ?', [stars, uuid], function (error, results, fields) {
+			return cb(error);
+		});
+	}
 };
