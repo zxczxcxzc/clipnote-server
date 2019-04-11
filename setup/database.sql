fe26ce5d66a5bbbce8a9b9e7430b690e2025fcb6
user <51wanderer51@gmail.com>
2019-04-10 20:43:36 -0400

    nickname is not null by default

--- setup/database.sql
+++ setup/database.sql
@@ -34,7 +34,7 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `uuid` varchar(45) NOT NULL,
   `permissions` int(1) NOT NULL DEFAULT '1',
-  `nick` varchar(45) DEFAULT NULL,
+  `nick` varchar(45) NOT NULL,
   `stars` int(11) DEFAULT '0',
   `joinDate` date NOT NULL,
   `hash` varchar(99) NOT NULL,
