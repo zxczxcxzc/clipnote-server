521108c0a32a348739b3d5e0e8ab3cc7be123a2a
user <51wanderer51@gmail.com>
2019-04-10 20:39:05 -0400

    use int for id (removed clipnote ids) and add uuid

--- setup/database.sql
+++ setup/database.sql
@@ -31,7 +31,8 @@ CREATE TABLE `notes` (
 DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `users` (
-  `id` varchar(45) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(45) NOT NULL,
   `permissions` int(1) NOT NULL DEFAULT '1',
   `nick` varchar(45) DEFAULT NULL,
   `stars` int(11) DEFAULT '0',
